d09cae03f91a3752fb91cc7c8fd10c58a0c8a7cd
jjmoncus <jjmoncus706@gmail.com>
2025-10-14 17:20:15 +0100

    add find_batts

--- R/find.R
+++ R/find.R
@@ -35,3 +35,43 @@ find_level <- function(data, pattern) {
     }
   })
 }
+
+
+#' Make educated guesses for which variables pertain to the same battery
+#'
+#' @export
+find_batts <- function(data) {
+
+  meta(data)$variable_label %>%
+    map_chr(~str_extract(.x, "^Q[[:digit:]]+")) %>%
+    {tibble(var = names(.),
+            cat = .)} %>%
+    split(~cat) %>%
+    map(~pull(.x, var)) %>%
+    # break apart the groups whose members dont all have the same levels
+    imap(function(vec, name) {
+
+      levels_out <- vec %>%
+        set_names() %>%
+        map(~data[[.x]] %>% levels())
+
+      if (length(unique(levels_out)) > 1) {
+
+        levels_out_sub <- unique(levels_out)
+
+        map(levels_out_sub, function(sub) {
+          keep(levels_out, ~identical(.x, sub)) %>%
+            names()
+        }) %>%
+          set_names(1:length(levels_out_sub))
+
+      } else {
+        names(levels_out)
+      }
+    }) %>%
+    list_flatten() %>%
+    keep(~length(.x) > 1)
+
+
+}
+
